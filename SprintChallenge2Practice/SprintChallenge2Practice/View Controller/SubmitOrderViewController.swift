4518b79c5af951b3df24380df6e38032e54ec6d9
Hayden Hastings <haydenhastings32@gmail.com>
2019-08-09 16:38:33 -0600

    Completed the app.

--- SprintChallenge2Practice/SprintChallenge2Practice/View Controller/SubmitOrderViewController.swift
+++ SprintChallenge2Practice/SprintChallenge2Practice/View Controller/SubmitOrderViewController.swift
@@ -9,29 +9,38 @@
 import UIKit
 
 class SubmitOrderViewController: UIViewController {
+    
+    // MARK: - Properties
+    var shoppingItemController: ShoppingItemsController?
 
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
     
-    // MARK: - Properties
-    var shoppingItemController: ShoppingItemsController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        message()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Methods
+    func message() {
+        if let numberOfItems = shoppingItemController?.selectedItems.count {
+            messageLabel.text = "You currently have \(numberOfItems) item's in your shopping list."
+        }
     }
-    */
-
+    
+    func submiteButton() {
+        let alert = UIAlertController(title: "Submited", message: "Thanks \(nameTextField.text!)! The items you have added will be delived to your address at \(addressTextField.text!) in 15 Minutes", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: {_ in let alert = UIAlertController(title: "Thank You", message: "You have successfully submitted your order.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: {_ in self.navigationController?.popViewController(animated: true)}))
+            self.present(alert, animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - IBActions
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        submiteButton()
+    }
 }
